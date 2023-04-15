dd37f42af54ab32acd8385156cc4e58b66405ac8
Anya Cherkasova <a.c4erkasova@gmail.com>
2023-04-15 09:30:01 +0300

    solved a new one

--- task.js
+++ task.js
@@ -1,9 +1,27 @@
-var number = function (array) {
-    let arr = []
-    for (i = 0; i < array.length; i++) {
-        arr.push(`${i + 1}` + ': ' + `${array[i]}`);
+function isValidWalk(walk) {
+    //walk.length == 10 
+    //'n' == 's'
+    //'w' == 'e' 
+    let nCount = 0;
+    let sCount = 0;
+    let wCount = 0;
+    let eCount = 0;
+    for (i = 0; i < walk.length; i++){
+        if (walk[i] === 'n'){
+            nCount ++; 
+        } else if (walk[i] === 's') {
+            sCount ++;
+        } else if (walk[i] === 'w') {
+            wCount ++;
+        } else {
+            eCount ++;
+        }
     }
-    return arr;
+    if(walk.length == 10 && nCount == sCount && wCount == eCount){
+        return true;
+    } else{
+        return false
+    }
 }
 
-console.log(number([]));
+    console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']));
