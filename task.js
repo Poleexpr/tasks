04676dc57a91491bb0c2615354253013537a6ef9
Anya Cherkasova <a.c4erkasova@gmail.com>
2023-05-15 14:28:02 +0300

    solved a new one

--- task.js
+++ task.js
@@ -1,10 +1,29 @@
-let user = {
-  name: 'John',
-  age: 30
-};
-
-function count(user){
-  return Object.values(user).length;
+function sumTo(n){
+let result = 0;
+for( let i = 0; i <= n; i++){
+  result += i;
+}
+return result;
 }
 
-console.log(count(user));
+console.log (sumTo(5));
+
+function sumToR(n){
+  if(n == 0){
+    return 0
+  } else if (n == 1){
+    return 1
+  } else{
+    return n + sumToR(n - 1);
+  }
+  }
+  
+  console.log (sumToR(5));
+
+
+  function sumToS(n){
+    return (1 + n) / 2 * n;
+    }
+    
+    console.log (sumToS(5));
+
