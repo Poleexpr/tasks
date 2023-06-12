2e8647adf81c8b56482baf18f81ad042616eea97
Anya Cherkasova <a.c4erkasova@gmail.com>
2023-06-12 13:38:07 +0300

    solved a new one

--- task.js
+++ task.js
@@ -1,13 +1,13 @@
-
-const runningSum = function(nums) {
-  const result = [];
-  const a = 0;
-  for (let i = 0; i < nums.length; i++){
-      a += nums[i];
-      result.push(a);
-  }
-  return result;   
+let containsDuplicate = function(nums) {
+    const arr = nums.sort()
+    for (let i = 0; i < arr.length; i++){
+      if(arr[i] == arr[i+1]){
+        return true
+      } else {
+        return false
+      }
+    }
 };
 
-console.log(runningSum([1,2,3,4]));
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
 
