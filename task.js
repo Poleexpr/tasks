e225f0377af4fe53dd3f6335fe7a028d43f5193c
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-01-30 08:54:12 +0300

    feat: solve a problem

--- task.js
+++ task.js
@@ -1,11 +1,5 @@
-"use strict";
-function dominantIndex(nums) {
-    const copyNums = nums.slice();
-    const sortedNums = copyNums.sort((a, b) => b - a);
-    const largestIntegerArr = sortedNums.slice(0, 1);
-    const largestInteger = +largestIntegerArr;
-    const arr = sortedNums.slice(1);
-    const result = arr.every(a => a * 2 <= largestInteger);
-    return result ? nums.indexOf(largestInteger) : -1;
+function plusOne(digits) {
+    var result = BigInt(digits.join('')) + BigInt(1);
+    return result.toString().split('');
 }
-console.log(dominantIndex([3, 6, 1, 0]));
+console.log(plusOne([3, 6, 1, 0]));
