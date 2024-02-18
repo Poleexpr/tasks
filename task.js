498ea97c1fb9cffffcd8dbed326a1250900bc6cb
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-02-18 12:06:07 +0300

    feat: solve a new problem

--- task.js
+++ task.js
@@ -1,15 +1,26 @@
-function createCounter(init) {
-    return {
-        increment: function () {
-            return init + 1;
-        },
-        reset: function () {
-            return init;
-        },
-        decrement: function () {
-            return init - 1;
-        },
+function curry(f) {
+    return function (a) {
+        return function (b) {
+            return f(a, b);
+        };
     };
 }
-var counter = createCounter(0);
-console.log(counter.increment());
+function filter(arr, fn) {
+    var result = [];
+    var curriedFn = curry(fn);
+    console.log(curriedFn);
+    for (var i = 0; i < arr.length; i++) {
+        if (curriedFn(arr[i])) {
+            result.push(arr[i]);
+        }
+        if (curriedFn(i)) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+var arr = [0, 10, 20, 30];
+function greaterThan10(n) {
+    return n > 10;
+}
+console.log(filter(arr, greaterThan10));
--- task.ts
+++ task.ts
@@ -1,24 +1,34 @@
-type Counter = {
-	increment: () => number
-	decrement: (init: number) => number
-	reset: (init: number) => number
-}
+type Fn = (n: number, i: number) => any
 
-function createCounter(init: number): Counter {
-	let i = init
-	return {
-		increment: () => {
-			return ++i
-		},
-		reset: () => {
-			return init
-		},
-		decrement: () => {
-			return --i
-		},
+function curry(f: Fn) {
+	return function (a: number) {
+		return function (b: number) {
+			return f(a, b)
+		}
 	}
 }
 
-const counter = createCounter(0)
+function filter(arr: number[], fn: Fn): number[] {
+	const result = []
+	let curriedFn = curry(fn)
 
-console.log(counter.increment())
+	console.log(curriedFn)
+	for (let i = 0; i < arr.length; i++) {
+		if (curriedFn(arr[i])) {
+			result.push(arr[i])
+		}
+		if (curriedFn(i)) {
+			result.push(i)
+		}
+	}
+
+	return result
+}
+
+let arr = [0, 10, 20, 30]
+
+function greaterThan10(n: number) {
+	return n > 10
+}
+
+console.log(filter(arr, greaterThan10))
