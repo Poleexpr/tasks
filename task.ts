252347e04e312139143872c5f93528b9459a10f0
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-02-19 13:43:28 +0300

    feat: solve a new problem

--- task.ts
+++ task.ts
@@ -3,7 +3,6 @@ type Fn = (accum: number, curr: number) => number
 function reduce(nums: number[], fn: Fn, init: number): number {
 	for (let i = 0; i < nums.length; i++) {
 		init = fn(init, nums[i])
-		console.log(init)
 	}
 
 	return init
