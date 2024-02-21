5fe86f295e0c38c5abe60e95d0b3408552d48217
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-02-21 12:40:41 +0300

    feat: solve a new problem

--- task.js
+++ task.js
@@ -1,8 +1,31 @@
-function argumentsLength() {
-    var args = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        args[_i] = arguments[_i];
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
     }
-    return args.length;
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function memoize(fn) {
+    var obj = {};
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        var json = JSON.stringify(__spreadArray([], args, true));
+        if (obj.hasOwnProperty(json)) {
+            return obj[json];
+        }
+        else {
+            obj[json] = fn.apply(void 0, args);
+            return obj[json];
+        }
+    };
 }
-;
+var sum = function (a, b) {
+    return a + b;
+};
+var memoizedSum = memoize(sum);
+console.log(memoizedSum(2, 2));
--- task.ts
+++ task.ts
@@ -1,5 +1,21 @@
-type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Fn = (...params: number[]) => number
 
-function argumentsLength(...args: JSONValue[]): number {
-    return args.length
-};
+function memoize(fn: Fn): Fn {
+	const obj = {}
+	return function (...args) {
+		let json = JSON.stringify([...args])
+		if (obj.hasOwnProperty(json)) {
+			return obj[json]
+		} else {
+			obj[json] = fn(...args)
+			return obj[json]
+		}
+	}
+}
+
+const sum: Fn = (a, b) => {
+	return a + b
+}
+const memoizedSum = memoize(sum)
+
+console.log(memoizedSum(2, 2))
