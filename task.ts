c95263812ba9acad097f8b824202fd86dc1a65cb
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-02-28 18:38:10 +0300

    feat: solve a new problem

--- task.ts
+++ task.ts
@@ -1,3 +1,20 @@
-async function sleep(millis: number): Promise<void> {
-	await new Promise(resolve => setTimeout(resolve, millis))
+type JSONValue =
+	| null
+	| boolean
+	| number
+	| string
+	| JSONValue[]
+	| { [key: string]: JSONValue }
+type Fn = (...args: JSONValue[]) => void
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+	const timeoutId = setTimeout(() => {
+		fn(...args)
+	}, t)
+
+	const cancelFn: Function = () => {
+		clearTimeout(timeoutId)
+	}
+
+	return cancelFn
 }
