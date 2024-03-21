90a03bcf072276aae5a9944db0f1c70e95b469e7
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-03-21 13:09:04 +0300

    feat: solve a new problem

--- task.ts
+++ task.ts
@@ -1,22 +1,12 @@
-var promiseAll = function (functions: any) {
-	return new Promise((resolve, reject) => {
-		if (functions.length === 0) {
-			resolve([])
-			return
-		}
-		const res: any[] = []
-		let resolvedCount = 0
-		functions.forEach(async (el: any, idx: any) => {
-			try {
-				const subResult = await el()
-				res[idx] = subResult
-				resolvedCount++
-				if (resolvedCount === functions.length) {
-					resolve(res)
-				}
-			} catch (err) {
-				reject(err)
-			}
-		})
-	})
+type JSONValue =
+	| null
+	| boolean
+	| number
+	| string
+	| JSONValue[]
+	| { [key: string]: JSONValue }
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+	return Object.keys(obj).length === 0
 }
