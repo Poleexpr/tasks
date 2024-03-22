0e2cb6577db445bac4e3784859a058f218a6e991
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-03-22 16:17:36 +0300

    feat: solve a new problem

--- task.ts
+++ task.ts
@@ -5,8 +5,16 @@ type JSONValue =
 	| string
 	| JSONValue[]
 	| { [key: string]: JSONValue }
-type Obj = Record<string, JSONValue> | JSONValue[]
+type Obj = Record<string, JSONValue> | Array<JSONValue>
 
-function isEmpty(obj: Obj): boolean {
-	return Object.keys(obj).length === 0
+function chunk(arr: number[], size: number): Obj[][] | any {
+	const result = []
+
+	for (let i = 0; i < arr.length; i += size) {
+		result.push(arr.slice(i, i + size))
+	}
+
+	return result
 }
+
+console.log(chunk([1, 2, 3, 4, 5], 1))
