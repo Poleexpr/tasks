236dd1e4197101f4587e0886369d04d53e3f4c1b
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-04-04 14:56:12 +0300

    feat: solve a new problem

--- task.js
+++ task.js
@@ -1,8 +1,12 @@
-function merge(nums1, m, nums2, n) {
-    var arr1 = nums1.slice(0, m);
-    var arr2 = nums2.slice(0, n);
-    console.log(nums1.slice(0, m));
-    nums1 = arr1.concat(arr2).sort(function (a, b) { return a - b; });
-    return nums1;
+function removeElement(nums, val) {
+    var k = 0;
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
 }
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
--- task.ts
+++ task.ts
@@ -1,14 +1,13 @@
-function merge(
-	nums1: number[],
-	m: number,
-	nums2: number[],
-	n: number
-): number[] {
-	const arr1 = nums1.slice(0, m)
-	const arr2 = nums2.slice(0, n)
-	console.log(nums1.slice(0, m))
-	nums1 = arr1.concat(arr2).sort((a, b) => a - b)
-	return nums1
+function removeElement(nums: number[], val: number): number {
+	let k = 0
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== val) {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+	return k
 }
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+console.log(removeElement([3, 2, 2, 3], 3))
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
