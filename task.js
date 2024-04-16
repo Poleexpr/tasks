36920069db7eea019ab300852be22aad4cec0404
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-04-16 16:00:10 +0300

    feat: solve a new one

--- task.js
+++ task.js
@@ -1,12 +1,5 @@
-function majorityElement(nums) {
-    var _a;
-    var map = new Map();
-    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
-        var num = nums_1[_i];
-        var g = ((_a = map.get(num)) !== null && _a !== void 0 ? _a : 0) + 1;
-        map.set(num, g);
-        if (g > nums.length / 2)
-            return num;
+function rotate(nums, k) {
+    while (k--) {
+        nums.unshift(nums.pop());
     }
 }
-console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
--- task.ts
+++ task.ts
@@ -1,10 +1,5 @@
-function majorityElement(nums: number[]): number {
-	const map = new Map()
-	for (let num of nums) {
-		let g = (map.get(num) ?? 0) + 1
-		map.set(num, g)
-		if (g > nums.length / 2) return num
+function rotate(nums: number[], k: number): void {
+	while (k--) {
+		nums.unshift(nums.pop())
 	}
 }
-
-console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]))
