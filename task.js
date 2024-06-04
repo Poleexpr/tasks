6737d7bcf6c063ac7689032ed8025e8337a841a9
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-04 16:28:48 +0300

    feat: solve a new one

--- task.js
+++ task.js
@@ -1,10 +1,6 @@
-function canJump(nums) {
-    var result = nums.length - 1;
-    for (var i = nums.length - 2; i >= 0; i--) {
-        if (nums[i] + i >= result) {
-            result = i;
-        }
+function rotate(nums, k) {
+    while (k--) {
+        nums.unshift(nums.pop());
     }
-    return result === 0;
 }
-console.log(canJump([2, 3, 1, 1, 4]));
+;
--- task.ts
+++ task.ts
@@ -1,12 +1,5 @@
-function canJump(nums: number[]): boolean {
-	let result = nums.length - 1
-
-	for (let i = nums.length - 2; i >= 0; i--) {
-		if (nums[i] + i >= result) {
-			result = i
-		}
-	}
-	return result === 0
-}
-
-console.log(canJump([2, 3, 1, 1, 4]))
+function rotate(nums: number[], k: number): void {
+    while(k--) {
+nums.unshift(nums.pop());
+    }
+};
