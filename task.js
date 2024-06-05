b050a2010c97066d756c9d7025cf1260bc187c1b
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-05 11:27:18 +0300

    feat: solve a new problem

--- task.js
+++ task.js
@@ -1,6 +1,11 @@
-function rotate(nums, k) {
-    while (k--) {
-        nums.unshift(nums.pop());
+function containsDuplicate(nums) {
+    var obj = {};
+    var k = 0;
+    for (var i = 0; i < nums.length; i++) {
+        obj.hasOwnProperty(nums[i]) ? (obj[nums[i]] += 1) : (obj[nums[i]] = 0);
     }
+    console.log(obj);
+    var more = function (m) { return m >= 1; };
+    return Object.values(obj).some(more);
 }
-;
+console.log(containsDuplicate([1, 2, 3, 1]));
--- task.ts
+++ task.ts
@@ -1,5 +1,12 @@
-function rotate(nums: number[], k: number): void {
-    while(k--) {
-nums.unshift(nums.pop());
-    }
-};
+function containsDuplicate(nums: number[]): boolean {
+	const obj = {}
+	let k = 0
+	for (let i = 0; i < nums.length; i++) {
+		obj.hasOwnProperty(nums[i]) ? (obj[nums[i]] += 1) : (obj[nums[i]] = 0)
+	}
+	console.log(obj)
+	const more = m => m >= 1
+	return Object.values(obj).some(more)
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]))
