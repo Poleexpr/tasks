2d3fd7cc4ac5de13bc1a231e86347f7d3ebd9537
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-10 12:22:53 +0300

    feat: solve a new one

--- task.js
+++ task.js
@@ -1,8 +1,19 @@
-function singleNumber(nums) {
-    var result = nums[0];
-    for (var i = 1; i < nums.length; i++) {
-        result = result ^ nums[i];
+function intersect(nums1, nums2) {
+    var result = [];
+    if (nums1.length < nums2.length) {
+        return intersect(nums2, nums1);
+    }
+    var obj = nums1.reduce(function (acc, num) {
+        acc[num] = acc[num] + 1 || 1;
+        console.log(acc);
+        return acc;
+    }, {});
+    for (var i = 0; i < nums2.length; i++) {
+        if (obj[nums2[i]] !== undefined && obj[nums2[i]] > 0) {
+            obj[nums2[i]] = obj[nums2[i]] - 1;
+            solution.push(nums2[i]);
+        }
     }
     return result;
 }
-console.log(singleNumber([1, 2, 3, 1]));
+console.log(intersect([1, 2, 2, 1], [2, 2]));
--- task.ts
+++ task.ts
@@ -1,10 +1,22 @@
-function singleNumber(nums: number[]): number {
-	let result = nums[0]
-	for (let i = 1; i < nums.length; i++) {
-		result = result ^ nums[i]
+function intersect(nums1: number[], nums2: number[]): number[] {
+	let result = []
+	if (nums1.length < nums2.length) {
+		return intersect(nums2, nums1)
+	}
+	const obj = nums1.reduce((acc, num) => {
+		// здесь сначала будет underfund тк ключ acc[num] отсутствует, он создаётся со значением 1, затем совпадения ключа считаются добавлением 1 каждый раз
+		acc[num] = acc[num] + 1 || 1
+		return acc
+	}, {})
+
+	for (let i = 0; i < nums2.length; i++) {
+		if (obj[nums2[i]] !== undefined && obj[nums2[i]] > 0) {
+			obj[nums2[i]] = obj[nums2[i]] - 1
+			result.push(nums2[i])
+		}
 	}
 
 	return result
 }
 
-console.log(singleNumber([1, 2, 3, 1]))
+console.log(intersect([1, 2, 2, 1], [2, 2]))
