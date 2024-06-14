2e6ecc890ab44861951e3419e88fa8255a8c9f96
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-06-14 15:01:40 +0300

    feat: solve a new problem

--- task.js
+++ task.js
@@ -1,19 +1,8 @@
-function intersect(nums1, nums2) {
-    var result = [];
-    if (nums1.length < nums2.length) {
-        return intersect(nums2, nums1);
-    }
-    var obj = nums1.reduce(function (acc, num) {
-        acc[num] = acc[num] + 1 || 1;
-        console.log(acc);
-        return acc;
-    }, {});
-    for (var i = 0; i < nums2.length; i++) {
-        if (obj[nums2[i]] !== undefined && obj[nums2[i]] > 0) {
-            obj[nums2[i]] = obj[nums2[i]] - 1;
-            solution.push(nums2[i]);
+function moveZeroes(nums) {
+    for (var i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] === 0) {
+            nums.push(0);
+            nums.splice(i, 1);
         }
     }
-    return result;
 }
-console.log(intersect([1, 2, 2, 1], [2, 2]));
--- task.ts
+++ task.ts
@@ -1,22 +1,8 @@
-function intersect(nums1: number[], nums2: number[]): number[] {
-	let result = []
-	if (nums1.length < nums2.length) {
-		return intersect(nums2, nums1)
-	}
-	const obj = nums1.reduce((acc, num) => {
-		// здесь сначала будет underfund тк ключ acc[num] отсутствует, он создаётся со значением 1, затем совпадения ключа считаются добавлением 1 каждый раз
-		acc[num] = acc[num] + 1 || 1
-		return acc
-	}, {})
-
-	for (let i = 0; i < nums2.length; i++) {
-		if (obj[nums2[i]] !== undefined && obj[nums2[i]] > 0) {
-			obj[nums2[i]] = obj[nums2[i]] - 1
-			result.push(nums2[i])
+function moveZeroes(nums: number[]): void {
+	for (let i = nums.length - 1; i >= 0; i--) {
+		if (nums[i] === 0) {
+			nums.push(0)
+			nums.splice(i, 1)
 		}
 	}
-
-	return result
 }
-
-console.log(intersect([1, 2, 2, 1], [2, 2]))
